47875c23a500aae4793866bd5db8313478133190
Stephan Keel <git@zopf.ch>
2016-09-18 20:43:50 +0200

    single page version

--- notes.js
+++ notes.js
@@ -2,19 +2,29 @@
  * Created by Stephan on 17.09.2016.
  */
 
+function activateMain() {
+    document.getElementById("mainSection").style.display = "inline";
+    document.getElementById("editSection").style.display = "none";
+    document.getElementById("mainTitle").innerHTML = "Notes - Overview";
+}
 
+function activateEdit() {
+    document.getElementById("mainSection").style.display = "none";
+    document.getElementById("editSection").style.display = "inline";
+    document.getElementById("mainTitle").innerHTML = "Notes - Edit";
+}
+
+function saveNote() {
+    // Store the note
+    // TODO
+
+    // Activate the main page again
+    activateMain();
+}
 
 function selectCSS(id, name){
     document.getElementById(id).setAttribute('href', name);
     log("selectCSS: " + id + ", " + name + " --> " + document.getElementById(id).getAttribute('href'));
-}
-
-function getCurrentCSS() {
-    return t.getElementById("style").getAttribute("href");
-}
-
-function createNewNote() {
-    log("createNewNote clicked");
 }
 
 function orderByCompletionDate() {
@@ -34,9 +44,6 @@ function filterNotes(value) {
 }
 
 function log(text) {
-    var dst = document.getElementById("placeholder");
-    if (!dst) {
-        dst = document.getElementById("logArea");
-    }
+    var dst = document.getElementsByClassName("logArea")[0];
     dst.innerHTML = text;
 }
