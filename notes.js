ddbbd161bde980bfd4e43d6d9f2aa4d25e460599
stephankeel <stephan.keel@hsr.ch>
2016-09-29 21:55:07 +0200

    save selected css in local storage

--- notes.js
+++ notes.js
@@ -2,6 +2,19 @@
  * Created by Stephan on 17.09.2016.
  */
 
+var CSS_STORAGE_KEY = "notes-css";
+
+log("jquery version " + $.fn.jquery);
+init();
+
+function init() {
+    var storedCSS = localStorage.getItem(CSS_STORAGE_KEY);
+    if (storedCSS) {
+        var selectedCSS = JSON.parse(storedCSS);
+        selectCSS(selectedCSS.id, selectedCSS.name, false);
+    }
+}
+
 function activateMain() {
     $("#mainSection").show();
     $("#editSection").hide();
@@ -29,9 +42,15 @@ function cancelEdit() {
     activateMain();
 }
 
-function selectCSS(id, name) {
+function selectCSS(id, name, save=true) {
     document.getElementById(id).setAttribute('href', name);
-    log("selectCSS: " + id + ", " + name + " --> " + document.getElementById(id).getAttribute('href'));
+    if (save) {
+        var selectedCSS = {id: id, name: name};
+        localStorage.setItem(CSS_STORAGE_KEY, JSON.stringify(selectedCSS));
+    } else {
+        document.getElementById("styleSelection").value = name;
+    }
+    log("selectCSS: " + id + ", " + name + " --> " + document.getElementById(id).getAttribute('href') + ", saved: " + save);
 }
 
 function orderByCompletionDate() {
