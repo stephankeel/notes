82863e296f5006191784477d25fde7cf7035e071
Stephan Keel <git@zopf.ch>
2016-11-03 17:19:38 +0100

    fix js lib path

--- public/scripts/noteInit.js
+++ public/scripts/noteInit.js
@@ -2,16 +2,17 @@
     'use strict';
 
 // if not available from the internet or no internet available then load it from the local disk
-    win.jQuery || doc.write('<script src="../external/jquery-3.1.1.min.js"><\/script>');
-    win.Handlebars || doc.write('<script src="../external/handlebars-v4.0.5.js"><\/script>');
-    win.moment || doc.write('<script src="../external/moment-2.15.1.min.js"><\/script>');
+    win.jQuery || doc.write('<script src="./external/jquery-3.1.1.min.js"><\/script>');
+    win.Handlebars || doc.write('<script src="./external/handlebars-v4.0.5.js"><\/script>');
+    win.moment || doc.write('<script src="./external/moment-2.15.1.min.js"><\/script>');
 
 // load the project scripts themselves
-    doc.write('<script src="../scripts/note.js"><\/script>');
-    doc.write('<script src="../scripts/localDataService.js"><\/script>');
-    doc.write('<script src="../scripts/remoteDataService.js"><\/script>');
-    doc.write('<script src="../scripts/noteModel.js"><\/script>');
-    doc.write('<script src="../scripts/noteRenderer.js"><\/script>');
-    doc.write('<script src="../scripts/noteController.js"><\/script>');
+    var root = window.location.pathname;
+    doc.write('<script src="./scripts/note.js"><\/script>');
+    doc.write('<script src="./scripts/localDataService.js"><\/script>');
+    doc.write('<script src="./scripts/remoteDataService.js"><\/script>');
+    doc.write('<script src="./scripts/noteModel.js"><\/script>');
+    doc.write('<script src="./scripts/noteRenderer.js"><\/script>');
+    doc.write('<script src="./scripts/noteController.js"><\/script>');
 
 }(window, document));
